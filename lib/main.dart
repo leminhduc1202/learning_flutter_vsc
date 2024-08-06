ab6b44afb31b6790216e047defc24b933ed2be6c
LeMinhDucDN <duc.le@madison-technology.com>
2024-08-06 14:27:51 +0700

    Bottomnavigationbar widget in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ void main() {
   // runApp(const MyApp());
   // runApp(const MyAppNew());
   // runApp(const ContainerView());
-  runApp(const ScaffoldWidgetView());
+  // runApp(const ScaffoldWidgetView());
+  runApp(const BottomNavigationbarWidget());
 }
 
 class MyApp extends StatelessWidget {
@@ -182,6 +183,84 @@ class ScaffoldView extends StatelessWidget {
         currentIndex: 0,
         fixedColor: Colors.green,
         onTap: (int indexOfitem) {},
+      ),
+    );
+  }
+}
+
+class BottomNavigationbarWidget extends StatelessWidget {
+  static const String _title = 'Learning Flutter Code Sample';
+  const BottomNavigationbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: NavigationView(),
+    );
+  }
+}
+
+class NavigationView extends StatefulWidget {
+  const NavigationView({super.key});
+
+  @override
+  State<NavigationView> createState() => _NavigationViewState();
+}
+
+class _NavigationViewState extends State<NavigationView> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'HOME PAGE',
+      style: optionStyle,
+    ),
+    Text(
+      'COURSE PAGE',
+      style: optionStyle,
+    ),
+    Text(
+      'CONTACT GFG',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GeeksForGeeks'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: "Courses",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_mail),
+            label: "Mail",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
