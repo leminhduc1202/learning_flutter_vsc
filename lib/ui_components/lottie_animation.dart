643682ccae04e15ff8865860e9a3ee736ac6c045
LeMinhDucDN <duc.le@madison-technology.com>
2024-08-16 14:04:34 +0700

    Google fonts in Flutter

--- lib/ui_components/lottie_animation.dart
+++ lib/ui_components/lottie_animation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
 class LottiePage extends StatefulWidget {
@@ -22,26 +23,42 @@ class LottiePageState extends State<LottiePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Lottie.network(
-              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
-              repeat: true,
-              reverse: true,
-              animate: true,
+            Text(
+              "Font google 1",
+              style: GoogleFonts.oswald(
+                  textStyle: const TextStyle(fontSize: 24.0)),
+            ),
+            Text(
+              "Font google 2",
+              style: GoogleFonts.zcoolKuaiLe(
+                  textStyle: const TextStyle(fontSize: 42.0)),
+            ),
+            Text(
+              "Font google 3",
+              style: GoogleFonts.daiBannaSil(
+                  textStyle: const TextStyle(fontSize: 36.0)),
             ),
             Lottie.network(
-              'https://assets1.lottiefiles.com/private_files/lf30_yQtj4O.json',
-              repeat: true,
-              reverse: true,
-              animate: true,
-            ),
+                'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
+                repeat: true,
+                reverse: true,
+                animate: true,
+                width: 100.0,
+                height: 100.0),
             Lottie.network(
-              'https://lottie.host/834e400c-7ade-4464-a7c8-d0033e70773c/KNA8BJvt4k.json',
-              repeat: true,
-              reverse: true,
-              animate: true,
-              width: 100.0,
-              height: 100.0
-            ),
+                'https://assets1.lottiefiles.com/private_files/lf30_yQtj4O.json',
+                repeat: true,
+                reverse: true,
+                animate: true,
+                width: 100.0,
+                height: 100.0),
+            Lottie.network(
+                'https://lottie.host/834e400c-7ade-4464-a7c8-d0033e70773c/KNA8BJvt4k.json',
+                repeat: true,
+                reverse: true,
+                animate: true,
+                width: 100.0,
+                height: 100.0),
           ],
         ),
       ),
