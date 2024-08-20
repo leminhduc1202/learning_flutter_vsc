33f2dedf3bfd8a7af9d7083aa73a195ebbb5db6b
LeMinhDucDN <duc.le@madison-technology.com>
2024-08-20 17:11:05 +0700

    Update data in Flutter

--- lib/navigation_routing_page/avoiding_junk.dart
+++ lib/navigation_routing_page/avoiding_junk.dart
@@ -12,9 +12,13 @@ class AvoidingJunk extends StatelessWidget {
   Widget build(BuildContext context) {
     const appTitle = 'Isolate Demo';
 
-    return const MaterialApp(
+    return MaterialApp(
       title: appTitle,
-      home: HomePage(title: appTitle),
+      initialRoute: '/',
+      routes: {
+        "/": (context) => const UpdateAlbum(),
+        "/list": (context) => const HomePage(title: appTitle),
+      },
     );
   }
 }
@@ -80,6 +84,139 @@ Widget itemPhoto(String photoUrl, String photoTitle) {
 }
 
 String url = "https://jsonplaceholder.typicode.com/photos";
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  if (response.statusCode == 200) {
+    return Album.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final http.Response response = await http.put(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+  if (response.statusCode == 200) {
+    return Album.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to update album.');
+  }
+}
+
+class UpdateAlbum extends StatefulWidget {
+  const UpdateAlbum({super.key});
+
+  @override
+  UpdateAlbumState createState() {
+    return UpdateAlbumState();
+  }
+}
+
+class UpdateAlbumState extends State<UpdateAlbum> {
+  final TextEditingController _controller = TextEditingController();
+  late Future<Album> _futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Update Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Load Data'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('List Album'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/list');
+              },
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(8.0),
+              child: FutureBuilder<Album>(
+                future: _futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(snapshot.data!.title),
+                          TextField(
+                            controller: _controller,
+                            decoration:
+                                const InputDecoration(hintText: 'Enter Title'),
+                          ),
+                          ElevatedButton(
+                            child: const Text('Update Data'),
+                            onPressed: () {
+                              setState(() {
+                                _futureAlbum = updateAlbum(_controller.text);
+                              });
+                            },
+                          ),
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+                  }
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Album {
+  final int id;
+  final String title;
+
+  Album({required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+Future<List<Album>> getAlbums(http.Client client) async {
+  final response = await client
+      .get(Uri.parse("https://jsonplaceholder.typicode.com/albums/"));
+  return compute(albumsParser, response.body);
+}
+
+List<Album> albumsParser(String responseBody) {
+  final parser = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parser.map<Album>((json) => Album.fromJson(json)).toList();
+}
 
 Future<List<PhotoData>> getPhotos(http.Client client) async {
   final response = await client.get(Uri.parse(url));
